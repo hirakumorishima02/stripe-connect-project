e9b06c2d8fe87ba153bb48724b9997045805e0aa
EC2 Default User <ec2-user@ip-172-31-28-88.ap-southeast-1.compute.internal>
2019-08-14 00:58:21 +0000

    processing Stripe connect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,4 +61,42 @@ class HomeController extends Controller
         }
 
     }
+    public function connect(){//子ユーザーコネクト作成
+           define('CLIENT_ID', 'ca_FQxcDLdqv9G6GTxLkBJGj9VuoG9OFk51');//connectの設置ページにある
+           define('TOKEN_URI', 'https://connect.stripe.com/oauth/token');
+           define('AUTHORIZE_URI', 'https://connect.stripe.com/oauth/authorize');
+           if (isset($_GET['code'])) { // Redirect/ code
+             $code = $_GET['code'];
+             $token_request_body = array(
+               'client_secret' => env('STRIPE_SECRET'),
+               'grant_type' => 'authorization_code',
+               'client_id' => CLIENT_ID,
+               'code' => $code,
+             );
+             $req = curl_init(TOKEN_URI);
+             curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+             curl_setopt($req, CURLOPT_POST, true );
+             curl_setopt($req, CURLOPT_POSTFIELDS, http_build_query($token_request_body));
+             // TODO: Additional error handling
+             $respCode = curl_getinfo($req, CURLINFO_HTTP_CODE);
+             $resp = json_decode(curl_exec($req), true);
+             curl_close($req);
+
+             $id = Auth::id();//user_id取得
+             \App\User::where('id', $id)->update(['stripe_user_id' => $resp['stripe_user_id']]);
+
+             return redirect()->back();
+           } else if (isset($_GET['error'])) { // Error
+             echo $_GET['error_description'];
+           } else { // Show OAuth link
+             $authorize_request_body = array(
+               'response_type' => 'code',
+               'scope' => 'read_write',
+               'client_id' => CLIENT_ID,
+             );
+             $url = AUTHORIZE_URI . '?' . http_build_query($authorize_request_body);
+
+             return view('connect')->with('url',$url);
+           }
+    }
 }
